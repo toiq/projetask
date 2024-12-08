7328e1de0cd837b6f5b310c674f16933da0b8bb5
toiq <tousif.iqbal@outlook.com>
2024-12-08 11:54:14 +0600

    Setup hono server

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,25 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+app.get("/task/:taskId", (c) => {
+  const { taskId } = c.req.param();
+  return c.json({
+    taskId,
+  });
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
+export const PUT = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
