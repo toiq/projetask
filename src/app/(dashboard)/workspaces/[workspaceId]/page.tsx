f5e1e2ef23599807b04609a12f9fa0924d207ccc
toiq <tousif.iqbal@outlook.com>
2024-12-17 20:35:26 +0600

    Fix constant redirect

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,5 +1,4 @@
 import { getCurrent } from "@/features/auth/actions";
-import { getWorkspaces } from "@/features/workspaces/actions";
 import { redirect } from "next/navigation";
 
 const WorkspaceIdPage = async () => {
@@ -8,15 +7,6 @@ const WorkspaceIdPage = async () => {
   if (!user) {
     redirect("/login");
   }
-
-  const workspaces = await getWorkspaces();
-
-  if (workspaces.total === 0) {
-    redirect("/workspaces/create");
-  } else {
-    redirect(`/workspaces/${workspaces.documents[0].$id}`);
-  }
-
   return <>Yes</>;
 };
 
