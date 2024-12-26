4fa6996a3f2e2fe3004fe6cd660d9416c9d60810
toiq <tousif.iqbal@outlook.com>
2024-12-26 15:20:00 +0600

    STYLE: Adjust layout of sidebar logo and reposition close button in sheet component

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,9 +8,11 @@ import Projects from "./projects";
 const Sidebar = () => {
   return (
     <aside className="h-full w-full bg-neutral-100 p-4">
-      <Link href="/">
-        <Logo />
-      </Link>
+      <div className="w-fit">
+        <Link href="/">
+          <Logo />
+        </Link>
+      </div>
       <Separator className="my-4" />
       <WorkspaceSwitcher />
       <Separator className="my-4" />
